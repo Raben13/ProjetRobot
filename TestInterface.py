9ffad1b1763f0de6905c04beb9204cb96d8e6a00
zeld4 <radhoane.ben@gmail.com>
2014-02-11 13:59:54 +0100

    Create TestInterface.py

---
+++ TestInterface.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+# script drag_and_drop.py
+
+from tkinter import *
+from interface import *
+
+fenetre = Tk()
+interface = Interface(fenetre)
+
+interface.mainloop()
+interface.destroy()
