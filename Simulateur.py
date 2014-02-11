029edc3df98d1d7d0a8eef87b8220363a58631bf
zeld4 <radhoane.ben@gmail.com>
2014-02-11 13:59:28 +0100

    Create Simulateur.py
    Description du simulateur (en cours )

---
+++ Simulateur.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+'''
+Created on 9 f�vr. 2014
+
+@author: Adam
+'''
+
+
+class Simulateur:
+    
+    
+    def __init__(self, x, y ,z):
+        self.X = x
+        self.Y = y
+        self.Z = z
